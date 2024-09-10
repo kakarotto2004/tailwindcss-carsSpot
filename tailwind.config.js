9a7bc44afbb1cdc12a002d81d17682eb221eb96a
Jakub Pastucha <kuba.pastucha@gmail.com>
2024-09-10 15:25:34 +0200

    style header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ['./*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                whiteBg: '#F7F7F7',
+                blueDark: '#0147FF',
+                grayDark: '#282828',
+            },
+        },
+
+        fontFamily: {
+            robotoFlex: ['Roboto Flex'],
+            bebas: ['Bebas Neue'],
+            robotoCondensed: ['Roboto Condensed'],
+        },
     },
     plugins: [],
 };
