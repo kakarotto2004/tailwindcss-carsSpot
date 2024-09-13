1cb0c09771a6bfd5d27971942e8eeeb1ce206130
Jakub Pastucha <kuba.pastucha@gmail.com>
2024-09-13 12:22:04 +0200

    make tablet styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,14 @@ module.exports = {
                 p88px: ['88px'],
             },
         },
+
+        screens: {
+            sm: '641px',
+            md: '769px',
+            lg: '1025px',
+            xl: '1281px',
+            '2xl': '1536px',
+        },
     },
     plugins: [],
 };
